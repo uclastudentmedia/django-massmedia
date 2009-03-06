f6be54568270c13dcf3dfd4d04eb650d0d643a13
jquick <>
2009-03-06 17:52:50 +0000

    added correct pickle field (thanks jose)

--- massmedia/models.py
+++ massmedia/models.py
@@ -84,30 +84,34 @@ def parse_metadata(path):
             data[key] = value
     return data
 
-class PickledObject(str): pass
-
 class PickledObjectField(models.Field):
-    __metaclass__ = models.SubfieldBase
-    
-    def to_python(self, value):
-        if isinstance(value, PickledObject): return pickle.loads(str(value))
-        else:
-            try: return pickle.loads(str(value))
-            except: return value
-    
-    def get_db_prep_save(self, value):
-        if value and not isinstance(value, PickledObject):
-            return PickledObject(pickle.dumps(value))
-        return value
-    
-    def get_internal_type(self): return 'TextField'
-    
-    def get_db_prep_lookup(self, lookup_type, value):
-        if lookup_type == 'exact': return super(PickledObjectField, self)\
-            .get_db_prep_lookup(lookup_type, self.get_db_prep_save(value))
-        elif lookup_type == 'in':  return super(PickledObjectField, self)\
-            .get_db_prep_lookup(lookup_type, [self.get_db_prep_save(v) for v in value])
-        else: raise TypeError('Lookup type %s is not supported.' % lookup_type)
+     """ Django snippet - http://www.djangosnippets.org/snippets/513/  """
+     __metaclass__ = models.SubfieldBase
+
+     def to_python(self, value):
+         try:
+             return pickle.loads(str(value))
+         except:
+             # If an error was raised, just return the plain value
+             return value
+
+     def get_db_prep_save(self, value):
+         if value is not None:
+             value = pickle.dumps(value)
+         return str(value)
+
+     def get_internal_type(self):
+         return 'TextField'
+
+     def get_db_prep_lookup(self, lookup_type, value):
+         if lookup_type == 'exact':
+             value = self.get_db_prep_save(value)
+             return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value)
+         elif lookup_type == 'in':
+             value = [self.get_db_prep_save(v) for v in value]
+             return super(PickledObjectField, self).get_db_prep_lookup(lookup_type, value)
+         else:
+             raise TypeError('Lookup type %s is not supported.' %  lookup_type)
 
 class Media(models.Model):
     title = models.CharField(max_length=255,unique=True)
