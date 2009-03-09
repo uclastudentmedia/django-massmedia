bf743ab643cf196fcaaf85d32ec087991afb135d
Weston Nielson <wnielson@weston>
2009-03-08 21:45:20 -0700

    Changed setup.py--Not a fan of disutils.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = "django-massmedia",
+    name='django-massmedia',
     version = '0.1',
-    url = 'http://opensource.washingtontimes.com/projects/django-massmedia/',
-    author = 'Justin Quick',
     description = 'Allows for site staff can upload and edit the media files through the site, and the filesystem is maintained in the background.',
-    packages = ['massmedia'],
+    author='Weston Nielson',
+    url='https://code.launchpad.net/~wnielson/django-massmedia/devel',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
     include_package_data=True,
     zip_safe=False,
     install_requires=['setuptools', 'setuptools_bzr'],
